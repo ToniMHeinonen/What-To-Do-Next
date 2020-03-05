1dd5acadd5721e8df7399cdd7c7ea4ee0076ed1f
Toni <toni.heinonen@hotmail.fi>
2020-03-05 12:25:13 +0200

    Add ListsActivity, SettingsActivity and VoteActivity.

---
+++ app/src/main/java/io/github/tonimheinonen/whattodonext/VoteActivity.java
@@ -0,0 +1,14 @@
+package io.github.tonimheinonen.whattodonext;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vote);
+    }
+}
