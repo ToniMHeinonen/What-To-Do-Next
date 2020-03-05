0fe3bbb1d38a90ad7c130875e4ddfc8a59547ca0
Toni <toni.heinonen@hotmail.fi>
2020-03-05 12:30:15 +0200

    Move to correct Activity when clicking Menu button.

--- app/src/main/java/io/github/tonimheinonen/whattodonext/MainActivity.java
+++ app/src/main/java/io/github/tonimheinonen/whattodonext/MainActivity.java
@@ -2,6 +2,8 @@ package io.github.tonimheinonen.whattodonext;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -13,13 +15,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    /**
+     * Starts selected activity.
+     * @param v clicked button
+     */
     public void menuButtonClicked(View v) {
+        Intent intent;
+
         if (v.getId() == R.id.voteButton) {
-
+            intent = new Intent(this, VoteActivity.class);
         } else if (v.getId() == R.id.listButton) {
-
+            intent = new Intent(this, ListsActivity.class);
         } else if (v.getId() == R.id.settingsButton) {
-
+            intent = new Intent(this, SettingsActivity.class);
+        } else {
+            return;
         }
+
+        startActivity(intent);
     }
 }
