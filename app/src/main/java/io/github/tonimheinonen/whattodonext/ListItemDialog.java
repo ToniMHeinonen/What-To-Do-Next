9010c926554979642bf63df0629aa84348b68e0c
Toni <toni.heinonen@hotmail.fi>
2020-03-05 19:26:01 +0200

    Modify item dialog box to show name, bonus and peril.

--- app/src/main/java/io/github/tonimheinonen/whattodonext/ListItemDialog.java
+++ app/src/main/java/io/github/tonimheinonen/whattodonext/ListItemDialog.java
@@ -1,4 +1,48 @@
 package io.github.tonimheinonen.whattodonext;
 
-public class ListItemDialog {
+import android.app.Activity;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+
+public class ListItemDialog extends Dialog implements
+        android.view.View.OnClickListener {
+
+    private Activity activity;
+    private ListItem item;
+
+    public ListItemDialog(Activity a, ListItem item) {
+        super(a);
+        this.activity = a;
+        this.item = item;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.list_item_dialog);
+        Button bonusMinus = findViewById(R.id.bonusMinus);
+        Button bonusPlus = findViewById(R.id.bonusPlus);
+        bonusMinus.setOnClickListener(this);
+        bonusPlus.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.bonusMinus:
+                activity.finish();
+                break;
+            case R.id.bonusPlus:
+                dismiss();
+                break;
+            default:
+                break;
+        }
+        dismiss();
+    }
 }
--- app/src/main/java/io/github/tonimheinonen/whattodonext/ListsActivity.java
+++ app/src/main/java/io/github/tonimheinonen/whattodonext/ListsActivity.java
@@ -2,6 +2,7 @@ package io.github.tonimheinonen.whattodonext;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -14,6 +15,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ListsActivity extends AppCompatActivity {
+
+    private Activity _this = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,7 +47,8 @@ public class ListsActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 ListItem item = (ListItem) list.getItemAtPosition(position);
-                System.out.println(item.toString());
+                ListItemDialog cdd=new ListItemDialog(_this, item);
+                cdd.show();
             }
         });
     }
