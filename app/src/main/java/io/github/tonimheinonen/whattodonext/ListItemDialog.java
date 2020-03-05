eb9170a00e4fe23f8ef7a56847bf0ac6510e78f0
Toni <toni.heinonen@hotmail.fi>
2020-03-05 20:06:01 +0200

    Make EditTexts and plus and minus work in ListItemDialog.

--- app/src/main/java/io/github/tonimheinonen/whattodonext/ListItemDialog.java
+++ app/src/main/java/io/github/tonimheinonen/whattodonext/ListItemDialog.java
@@ -3,15 +3,19 @@ package io.github.tonimheinonen.whattodonext;
 import android.app.Activity;
 import android.app.Dialog;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class ListItemDialog extends Dialog implements
         android.view.View.OnClickListener {
 
     private Activity activity;
     private ListItem item;
+    private EditText name, bonusAmount, perilAmount;
 
     public ListItemDialog(Activity a, ListItem item) {
         super(a);
@@ -24,25 +28,62 @@ public class ListItemDialog extends Dialog implements
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.list_item_dialog);
+
+        // Get item values
+        name = findViewById(R.id.itemName);
+        name.setText(item.getName());
+        bonusAmount = findViewById(R.id.bonusPoints);
+        perilAmount = findViewById(R.id.perilPoints);
+        updatePoints();
+
+        // Set listeners for + and - signs
         Button bonusMinus = findViewById(R.id.bonusMinus);
         Button bonusPlus = findViewById(R.id.bonusPlus);
+        Button perilMinus = findViewById(R.id.perilMinus);
+        Button perilPlus = findViewById(R.id.perilPlus);
         bonusMinus.setOnClickListener(this);
         bonusPlus.setOnClickListener(this);
+        perilMinus.setOnClickListener(this);
+        perilPlus.setOnClickListener(this);
+    }
 
+    private void getValuesFromEditTexts() {
+        String name = this.name.getText().toString();
+        item.setName(name);
+
+        int bonus = Integer.parseInt(bonusAmount.getText().toString());
+        item.setBonus(bonus);
+
+        int peril = Integer.parseInt(perilAmount.getText().toString());
+        item.setPeril(peril);
+    }
+
+    private void updatePoints() {
+        bonusAmount.setText(String.valueOf(item.getBonus()));
+        perilAmount.setText(String.valueOf(item.getPeril()));
     }
 
     @Override
     public void onClick(View v) {
+        getValuesFromEditTexts();
+
         switch (v.getId()) {
             case R.id.bonusMinus:
-                activity.finish();
+                item.setBonus(item.getBonus() - 1);
                 break;
             case R.id.bonusPlus:
-                dismiss();
+                item.setBonus(item.getBonus() + 1);
+                break;
+            case R.id.perilMinus:
+                item.setPeril(item.getPeril() - 1);
+                break;
+            case R.id.perilPlus:
+                item.setPeril(item.getPeril() + 1);
                 break;
             default:
                 break;
         }
-        dismiss();
+        updatePoints();
+        //dismiss();
     }
 }
