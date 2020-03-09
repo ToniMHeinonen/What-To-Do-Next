19c6b8da0c75cdacdc2f0a4357296a7c9985bc44
Toni <toni.heinonen@hotmail.fi>
2020-03-09 13:16:15 +0200

    Add custom colors, add color to selected sort method in lists.

--- app/src/main/java/io/github/tonimheinonen/whattodonext/ListsActivity.java
+++ app/src/main/java/io/github/tonimheinonen/whattodonext/ListsActivity.java
@@ -5,11 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.app.ListActivity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -24,11 +26,20 @@ public class ListsActivity extends AppCompatActivity {
     private final int NAME = 0, TOTAL = 1, BONUS = 2, PERIL = 3;
     private int curSort = NAME;
     private boolean ascending;
+    private TextView vName, vTotal, vBonus, vPeril;
+    private int originalTextColor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lists);
+
+        // Find topics so that color can be changed
+        vName = findViewById(R.id.name);
+        vTotal = findViewById(R.id.total);
+        vBonus = findViewById(R.id.bonus);
+        vPeril = findViewById(R.id.peril);
+        originalTextColor = vName.getCurrentTextColor();
 
         setupTestItems();
 
@@ -87,7 +98,27 @@ public class ListsActivity extends AppCompatActivity {
         sortList(sort);
     }
 
+    private void changeColor(int selected) {
+        vName.setTextColor(selected == NAME ?
+                getResources().getColor(R.color.textColorPrimary) :
+                originalTextColor);
+
+        vTotal.setTextColor(selected == TOTAL ?
+                getResources().getColor(R.color.textColorPrimary) :
+                originalTextColor);
+
+        vBonus.setTextColor(selected == BONUS ?
+                getResources().getColor(R.color.textColorPrimary) :
+                originalTextColor);
+
+        vPeril.setTextColor(selected == PERIL ?
+                getResources().getColor(R.color.textColorPrimary) :
+                originalTextColor);
+    }
+
     private void sortList(final int sort) {
+        changeColor(sort);
+
         // If new sort is same as current, reverse order
         if (sort == curSort) {
             ascending = !ascending;
