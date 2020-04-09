a4ac4b928b26cc947dea908ea8028fc122214e5b
Toni <toni.heinonen@hotmail.fi>
2020-04-09 09:00:57 +0300

    Add basic structure to SettingsActivity.

--- app/src/main/java/io/github/tonimheinonen/whattodonext/SettingsActivity.java
+++ app/src/main/java/io/github/tonimheinonen/whattodonext/SettingsActivity.java
@@ -3,6 +3,7 @@ package io.github.tonimheinonen.whattodonext;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 /**
  * Handles controlling of global values.
@@ -17,5 +18,29 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+    }
+
+    public void settingSelected(View v) {
+        switch (v.getId()) {
+            case R.id.maxPeril:
+
+                break;
+            case R.id.firstVote:
+
+                break;
+            case R.id.lastVote:
+
+                break;
+            case R.id.ignoreUnselected:
+
+                break;
+            case R.id.halfExtra:
+
+                break;
+        }
+    }
+
+    public void backSelected(View v) {
+        super.onBackPressed();
     }
 }
